bdaaae952489d9ea88a1343bbfac19d65133e778
Michael <michael.bretagne@gmail.com>
2018-08-06 11:12:36 -0700

    Add /blockchain /transasction /mine api endpoints

---
+++ dev/api.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/blockchain", (req, res) => {});
+
+app.post("/transaction", (req, res) => {});
+
+app.get("/mine", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
