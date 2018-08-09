2da2a3c5bc1d2a287753b79197c3029f6a8f02ae
Michael <michael.bretagne@gmail.com>
2018-08-08 22:42:48 -0700

    Build POST /register-nodes-bulk

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -105,7 +105,18 @@ app.post("/register-node", (req, res) => {
 });
 
 // register multiple nodes  at once
-app.post("/register-nodes-bulk", (req, res) => {});
+app.post("/register-nodes-bulk", (req, res) => {
+  const allNetworkNodes = req.body.allNetworkNodes;
+  allNetworkNodes.forEach(networkNodeUrl => {
+    const nodeNotAlreadyPresent =
+      bitcoin.networkNodes.indexOf(networkNodeUrl) === -1;
+    const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+    if (nodeNotAlreadyPresent && notCurrentNode) {
+      bitcoin.networkNodes.push(networkNodeUrl);
+    }
+  });
+  res.json({ note: "Bulk registration successful." });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
