75433a64c2b7d45d5e66a0840abeb131001acbe7
Michael <michael.bretagne@gmail.com>
2018-08-11 23:45:55 -0700

    Add chainIsValid method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -68,6 +68,49 @@ class Blockchain {
 
     return nonce;
   }
+
+  chainIsValid(blockchain) {
+    let validChain = true;
+
+    for (var i = 1; i < blockchain.length; i++) {
+      const currentBlock = blockchain[i];
+      const prevBlock = blockchain[i - 1];
+
+      const blockHash = this.hashBlock(
+        prevBlock["hash"],
+        {
+          transactions: currentBlock["transactions"],
+          index: currentBlock["index"],
+        },
+        currentBlock["nonce"],
+      );
+
+      if (blockHash.substring(0, 4) !== "0000") {
+        validChain = false;
+      }
+
+      if (currentBlock["previousBlockHash"] !== prevBlock["hash"]) {
+        validChain = false;
+      }
+    }
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock["nonce"] === 0;
+    const correctPreviousBlockHash = genesisBlock["previousBlockHash"] === "0";
+    const correctHash = genesisBlock["hash"] === "0";
+    const correctTransactions = genesisBlock["transactions"].length === 0;
+
+    if (
+      !correctNonce ||
+      !correctPreviousBlockHash ||
+      !correctHash ||
+      !correctTransactions
+    ) {
+      validChain = false;
+    }
+
+    return validChain;
+  }
 }
 
 module.exports = Blockchain;
